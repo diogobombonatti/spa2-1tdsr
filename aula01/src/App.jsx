5a55f82465f3db99e4eefa872eaa34614bf57a82
fbggbeholder <fbggbeholder@gmail.com>
2023-08-31 13:27:00 -0300

    commit 2

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+import viteLogo from "./assets/vite.svg";
+import Lista from "./components/Lista";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
 
-  return (
+  
+
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+    
+    {/* Criar um cabeçalho com um header, um h1 e uma lista ul + 3 elementos li. */}
+      <Lista/>
+    {/* Criar um elemento de conteudo com um section, uma div, dois parágrafos, cada um com 3 linhas de lorem e uma imagem. */}
+      <Outlet/>
+    {/* Criar um rodapé com uma div, uma lista ul e 3 elementos li com itens de link para redes sociais e um parágrafos com o seguinte texto : "Todos os meus direitos reservados - 2023", não esqueça de colocar o código do símbolo de copyright. */}
+      <Rodape/>
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
